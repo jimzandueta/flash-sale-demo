83c5100cf1f0739439948738c5043fc5977f0219
Jim Zandueta <jimzandueta@gmail.com>
2026-05-03 22:48:52 +0800

    feat: scaffold AWS terraform

---
+++ infra/envs/dev/variables.tf
@@ -0,0 +1,63 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "project_name" {
+  type    = string
+  default = "flash-sale-platform"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+variable "default_reservation_ttl_seconds" {
+  type    = number
+  default = 300
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "lambda_subnet_ids" {
+  type = list(string)
+}
+
+variable "lambda_security_group_ids" {
+  type = list(string)
+}
+
+variable "elasticache_subnet_ids" {
+  type = list(string)
+}
+
+variable "elasticache_security_group_ids" {
+  type = list(string)
+}
+
+variable "session_api_zip_path" {
+  type = string
+}
+
+variable "sales_api_zip_path" {
+  type = string
+}
+
+variable "reservation_api_zip_path" {
+  type = string
+}
+
+variable "checkout_api_zip_path" {
+  type = string
+}
+
+variable "reservation_worker_zip_path" {
+  type = string
+}
+
+variable "expiry_sweeper_zip_path" {
+  type = string
+}
